7b6d276fc56e56961339b9e42c10e7a8ed4a48a0
Milosch Meriac <milosch@meriac.com>
2015-07-12 00:10:41 +0100

    emit inner border

--- test.cpp
+++ test.cpp
@@ -12,6 +12,7 @@ typedef enum {BorderTop, BorderBottom} TBorderType;
 #define SHAPE_POINTS 100
 #define MALLOC_STEPS (1024UL*1024)
 #define HEADER_SIZE (80+sizeof(g_triangles))
+#define SHAPE_BORDER_WIDTH 5
 
 uint32_t g_triangles;
 
@@ -28,9 +29,12 @@ static void* add_stl(uint32_t bytes)
 
 	if((g_buffer_pos+bytes) > (g_buffer_size))
 	{
-		g_buffer = (uint8_t*) realloc(g_buffer, MALLOC_STEPS);
+		g_buffer = (uint8_t*) realloc(g_buffer, g_buffer_size+MALLOC_STEPS);
 		if(!g_buffer)
-			return NULL;
+		{
+			perror("Error: Failed to reallocate memory\n");
+			exit(1);
+		}
 
 		g_buffer_size += MALLOC_STEPS;
 	}
@@ -51,10 +55,10 @@ static void create_shape(double radius)
 	}
 }
 
-static void translate_shape(const Vector3d &translate, double progress_percent)
+static void translate_shape(const Vector3d &translate, int index)
 {
 	Matrix3d m;
-	m = AngleAxisd(M_PI/(SHAPE_POINTS/2),Vector3d::UnitZ()) * Scaling(1+0.01*progress_percent);
+	m = AngleAxisd(M_PI/(SHAPE_POINTS/2),Vector3d::UnitZ()) * Scaling(1+0.01*(((double)index)/SHAPE_POINTS));
 
 	for(int i=0; i<SHAPE_POINTS; i++)
 		g_target[i] = (m * g_shape[i]) + translate;
@@ -62,22 +66,31 @@ static void translate_shape(const Vector3d &translate, double progress_percent)
 
 static void dump_vector(const Vector3d &v)
 {
-	float *out = static_cast<float*>(add_stl(sizeof(float)*3));
+	float *out = static_cast<float*>(add_stl(sizeof(*out)*3));
 
 	for(int i=0; i<3; i++)
 		*out++ = v(i);
 }
 
+static void translate_inner_border(const Vector3d &vec, Vector3d &out)
+{
+	/* determine center */
+	Vector3d center(0, 0, g_shape[0](2));
+
+	Vector3d n = (center - vec).normalized();
+
+	out = vec + (n*SHAPE_BORDER_WIDTH);
+}
+
 static void emit_stl_vertex(const Vector3d &v1, const Vector3d &v2, const Vector3d &v3)
 {
-	Vector3d k1, k2, n;
+	Vector3d k1, k2;
 	uint16_t *attribute_count;
 
-	/* dump normale vector for inside */
+	/* dump normale vector for outside */
 	k1 = v2 - v1;
 	k2 = v3 - v1;
-	n = k1.cross(k2).normalized();
-	dump_vector(n);
+	dump_vector(k1.cross(k2).normalized());
 
 	/* dump vertexes */
 	dump_vector(v1);
@@ -92,12 +105,51 @@ static void emit_stl_vertex(const Vector3d &v1, const Vector3d &v2, const Vector
 	g_triangles++;
 }
 
-static void emit_stl_layer(void)
+void emit_stl_border(const Vector3d &v1, const Vector3d &v2, const Vector3d &v3)
 {
+	Vector3d k1, k2, border[3];
+	uint16_t *attribute_count;
+
+	/* dump normale vector for outside */
+	k1 = v2 - v1;
+	k2 = v3 - v1;
+	dump_vector(k1.cross(k2).normalized());
+
+	/* translate outer points to inner points */
+	translate_inner_border(v1, border[0]);
+	translate_inner_border(v2, border[1]);
+	translate_inner_border(v3, border[2]);
+
+	/* dump vertexes */
+	dump_vector(border[0]);
+	dump_vector(border[1]);
+	dump_vector(border[2]);
+
+	/* output empty attribute field */
+	attribute_count = static_cast<uint16_t*>(add_stl(sizeof(*attribute_count)));
+	*attribute_count = 0;
+
+	/* increment triangle count */
+	g_triangles++;
+}
+
+static void emit_stl_layer(bool border)
+{
+	Vector3d target2, shape2;
+
 	for(int i=0; i<SHAPE_POINTS; i++)
 	{
-		emit_stl_vertex(g_shape[i], g_target[i], g_target[(i+1)%SHAPE_POINTS]);
-		emit_stl_vertex(g_shape[i], g_target[(i+1)%SHAPE_POINTS], g_shape[(i+1)%SHAPE_POINTS]);
+		target2 = g_target[(i+1)%SHAPE_POINTS];
+		shape2 = g_shape[(i+1)%SHAPE_POINTS];
+
+		emit_stl_vertex(g_shape[i], g_target[i], target2);
+		emit_stl_vertex(g_shape[i], target2, shape2);
+
+		if(border)
+		{
+			emit_stl_border(target2,  g_target[i], g_shape[i]);
+			emit_stl_border(shape2, target2, g_shape[i]);
+		}
 	}
 }
 
@@ -112,7 +164,7 @@ static void emit_shaped_plane(void)
 
 static void emit_shaped_border(double border, TBorderType type)
 {
-	Vector3d p[4], v1, v2;
+	Vector3d p[4];
 
 	/* determine center */
 	Vector3d center(0, 0, g_shape[0](2));
@@ -122,11 +174,9 @@ static void emit_shaped_border(double border, TBorderType type)
 		p[0] = g_shape[i];
 		p[1] = g_shape[(i+1)%SHAPE_POINTS];
 
-		v1 = (center-p[0]).normalized()*border;
-		v2 = (center-p[1]).normalized()*border;
-
-		p[2] = p[0]+v1;
-		p[3] = p[1]+v2;
+		/* calculate inner border */
+		translate_inner_border(p[0], p[2]);
+		translate_inner_border(p[1], p[3]);
 
 		switch(type)
 		{
@@ -167,15 +217,11 @@ int main (int argc, char *argv[])
 	/* emit sculpture */
 	for(i=0; i<SHAPE_POINTS; i++)
 	{
-		/* calculate normalized translation vector */
-		Vector3d translate(0,0,1);
-		translate.normalize();
-
 		/* translate all points */
-		translate_shape(translate, ((double)i)/SHAPE_POINTS );
+		translate_shape(Vector3d::UnitZ(), i);
 
 		/* emit resulting layer */
-		emit_stl_layer();
+		emit_stl_layer(i>3);
 
 		/* move to next layer */
 		tmp = g_shape;
@@ -184,13 +230,13 @@ int main (int argc, char *argv[])
 
 		/* emit inner bottom */
 		if(i==3)
-			emit_shaped_border(5, BorderBottom);
+			emit_shaped_border(SHAPE_BORDER_WIDTH, BorderBottom);
 	}
 
 	/* emit top border */
-	emit_shaped_border(5, BorderTop);
+	emit_shaped_border(SHAPE_BORDER_WIDTH, BorderTop);
 
-	/* copy triangle coutn  to header */
+	/* copy triangle coun  to header */
 	memcpy(g_buffer + 80, &g_triangles, sizeof(g_triangles));
 
 	/* dump whole file */
@@ -198,4 +244,5 @@ int main (int argc, char *argv[])
 
 	return 0;
 }
+
 
